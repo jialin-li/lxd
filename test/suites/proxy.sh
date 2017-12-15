c7b22b8cfc1c62c0ca90fae6d25bc4d359388008
KishanRPatel <kishanrpatel@utexas.edu>
2017-12-14 21:33:30 -0600

    Finished proxy device integration tests
    Signed-off-by: KishanRPatel <kishanrpatel@utexas.edu>

--- test/suites/proxy.sh
+++ test/suites/proxy.sh
@@ -1,23 +1,84 @@
 #!/bin/bash
 
-shopt -s expand_aliases
-source ~/.bash_aliases
-
-add_proxy_device() {
-  lxc config device add $1 $2 proxy listen=tcp:127.0.0.1:$3 connect=tcp:127.0.0.1:$4 bind=$5
-}
-
 test_proxy_device() {
   
-  lxc launch ubuntu:16.04 proxyTest
-  add_proxy_device proxyTest test 1234 8888 host
-  sleep 1
-  lsc exec proxyTest -- nohup bash -c "nc -lkd 8888 > proxyTest.out &"
+  MESSAGE="Proxy device test string"
+
+  lxc launch ubuntu:16.04 proxyTester
+  lxc config device add proxyTester proxyDev proxy listen=tcp:127.0.0.1:1234 connect=tcp:127.0.0.1:4321 bind=host
+  if [[ $(lxc config device list proxyTester) != "proxyDev" ]]; then
+    echo "Proxy device was not added to container"
+  fi
+
+
+  lxc config device remove proxyTester proxyDev
+  if [[ $(lxc config device list proxyTester) ]]; then 
+    echo "Proxy device was not removed from container"
+  fi
+
+  lxc config device add proxyTester proxyDev proxy listen=tcp:127.0.0.1:1234 connect=tcp:127.0.0.1:4321 bind=host
+  lxc stop proxyTester
+  if [[ $(lxc config device list proxyTester) != "proxyDev" ]]; then
+    echo "Proxy device should not be deleted from config on container stop"
+  fi
+  lxc delete proxyTester
+
+  lxc launch ubuntu:16.04 proxyTester
+  if [[ $(lxc config device list proxyTester) ]]; then 
+    echo "Proxy device was not deleted from config on container deletion"
+  fi
+
+
+  lxc config device add proxyTester proxyDev proxy listen=tcp:127.0.0.1:1234 connect=tcp:127.0.0.1:4321 bind=host
+  sleep 1 
+
+  lxc exec proxyTester -- nohup bash -c "nc -lkd 4321 > proxyTest.out &"
   exec 3>/dev/tcp/localhost/1234
-  echo "Hello World, this your proxy device speaking!" >&3
+  echo ${MESSAGE} >&3
   sleep 1
-  #lsc exec proxyTest -- bash -c "exec 4</dev/tcp/localhost/8888"
-  #lsc exec proxyTest -- bash -c "cat <&4"
+  if [[ $(lxc exec proxyTester -- bash -c "cat proxyTest.out") != ${MESSAGE} ]]; then
+    echo "Proxy device did not properly send data from host to container"
+  fi
+
+  lxc config device remove proxyTester proxyDev
+  echo ${MESSAGE} >&3
+  sleep 1
+  if [[ $(lxc exec proxyTester -- bash -c "cat proxyTest.out") != ${MESSAGE} ]]; then
+    echo "Proxy device was not correctly stopped"
+  fi
+
+  lxc stop proxyTester
+  lxc delete proxyTester
+
+  lxc launch ubuntu:16.04 proxyTester
+  lxc config device add proxyTester proxyDev proxy listen=tcp:127.0.0.1:1234 connect=tcp:127.0.0.1:4321 bind=container
+  sleep 1
+  nc -lkd 4321 > proxyTest.out &
+  netcat=$!
+  lxc exec proxyTester -- bash -c "echo ${MESSAGE} > /dev/tcp/localhost/1234"
+  if [[ $(cat proxyTest.out) != ${MESSAGE} ]]; then
+    echo "Proxy device did not properly send data from container to host"
+  fi
+
+  kill $netcat
+  rm proxyTest.out
+
+  lxc restart proxyTester
+  sleep 1
+  nc -lkd 4321 > proxyTest.out &
+  netcat=$!
+  lxc exec proxyTester -- bash -c "echo ${MESSAGE} > /dev/tcp/localhost/1234"
+  if [[ $(cat proxyTest.out) != ${MESSAGE} ]]; then
+    echo "Proxy device did not restart on container restart"
+  fi
+  
+  kill $netcat
+  rm proxyTest.out
+
+
+  lxc stop proxyTester
+  lxc delete proxyTester
+
 }
 
 
